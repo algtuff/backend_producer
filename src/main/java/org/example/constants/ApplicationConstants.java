8015c5cdde3efafc703e00e6efd2d43513b496c8
andre <algtuff@gmail.com>
2024-10-07 17:47:54 -0300

    REF 999 - Ajuste queries

--- src/main/java/org/example/constants/ApplicationConstants.java
+++ src/main/java/org/example/constants/ApplicationConstants.java
@@ -13,10 +13,10 @@ public class ApplicationConstants {
     public static final Integer STUDIO_INDEX = 2;
     public static final Integer PRODUCERS_INDEX = 3;
     public static final Integer WINNER_INDEX = 4;
-    public static final String COLUMN_MAX_YEAR = "max_year";
-    public static final String COLUMN_MIN_YEAR = "min_year";
-    public static final String COLUMN_INTER = "inter";
-    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_MAX_YEAR = "following_win";
+    public static final String COLUMN_MIN_YEAR = "previous_win";
+    public static final String COLUMN_INTER = "diff";
+    public static final String COLUMN_NAME = "producer_name";
     public static final String CSV_SPLITTER = ";";
     public static final String MSG_MIGRATING_DATA ="Migrating data from CSV file to database...";
     public static final String MSG_DATA_MIGRATED = "Data migrated successfully!";
@@ -24,60 +24,107 @@ public class ApplicationConstants {
     public static final String EXCEPTION_PRODUCER_NOT_FOUND = "Producer not found";
 
     public static final String QUERY_WINNER_MAX_INTERVAL = """
-                select
-                        producer.name,
-                        max(movie.movie_year) as max_year,
-                        min(movie.movie_year) as min_year,
-                        max(movie.movie_year) -min(movie.movie_year) as inter
-                from producer as producer
-                         join movie_producers as mp on producer.id = mp.producers_id
-                         join movie as movie on mp.movie_movie_id = movie.movie_id
-                where
-                    movie.winner = true and
-                    producer.id in (select p.id
-                                        from producer as p
-                                             join movie_producers as m_p on p.id = m_p.producers_id
-                                             join movie as m on m_p.movie_movie_id = m.movie_id
-                                        where m.winner = true
-                                    group by p.id
-                                    having count(m.movie_id) > 1)
-                group by producer.name
-                having inter = (select max(inter) from
-                    (select max(m.movie_year)- min(m.movie_year) as inter
-                        from producer as p
-                              join movie_producers as m_p on p.id = m_p.producers_id
-                              join movie as m on m_p.movie_movie_id = m.movie_id
-                        where m.winner = true
-                     group by p.id
-                     having count(m.movie_id) > 1))""";
+        WITH victory_year AS (
+            SELECT
+                p.id AS producer_id,
+                p.name AS producer_name,
+                m.movie_year,
+                ROW_NUMBER() OVER (PARTITION BY p.id ORDER BY m.movie_year) AS vitoria_num
+            FROM
+                producer p
+                    JOIN
+                movie_producers mp ON p.id = mp.producers_id
+                    JOIN
+                movie m ON mp.movie_movie_id = m.movie_id
+            WHERE
+                m.winner = true
+        ),
+        differences AS (
+             SELECT
+                 producer_id,
+                 producer_name,
+                 movie_year,
+                 LAG(movie_year) OVER (PARTITION BY producer_id ORDER BY movie_year) AS previous_win
+             FROM
+                 victory_year
+        ),
+        intervals AS (
+            SELECT
+                 producer_id,
+                 producer_name,
+                 movie_year,
+                 previous_win,
+                 movie_year - previous_win AS inter
+            FROM
+                 differences
+            WHERE
+                 previous_win IS NOT NULL
+        )
+        SELECT
+            producer_name,
+            movie_year as following_win,
+            previous_win,
+            max(inter) as diff
+        FROM
+            intervals
+        group by
+            producer_id,
+            producer_name,
+            movie_year,
+            previous_win
+        having diff = select max(inter) from intervals
+        """;
 
     public static final String QUERY_WINNER_MIN_INTERVAL = """
-                select
-                    producer.name,
-                    max(movie.movie_year) as max_year,
-                    min(movie.movie_year) as min_year,
-                    max(movie.movie_year) -min(movie.movie_year) as inter
-                from producer as producer
-                         join movie_producers as mp on producer.id = mp.producers_id
-                         join movie as movie on mp.movie_movie_id = movie.movie_id
-                where
-                    movie.winner = true and
-                    producer.id in (select p.id
-                                        from producer as p
-                                             join movie_producers as m_p on p.id = m_p.producers_id
-                                             join movie as m on m_p.movie_movie_id = m.movie_id
-                                        where m.winner = true
-                                    group by p.id
-                                    having count(m.movie_id) > 1)
-                group by producer.name
-                having inter = (select min(inter) from
-                    (select max(m.movie_year)- min(m.movie_year) as inter
-                        from producer as p
-                              join movie_producers as m_p on p.id = m_p.producers_id
-                              join movie as m on m_p.movie_movie_id = m.movie_id
-                        where m.winner = true
-                     group by p.id
-                     having count(m.movie_id) > 1))""";
+        WITH victory_year AS (
+            SELECT
+                p.id AS producer_id,
+                p.name AS producer_name,
+                m.movie_year,
+                ROW_NUMBER() OVER (PARTITION BY p.id ORDER BY m.movie_year) AS vitoria_num
+            FROM
+                producer p
+                    JOIN
+                movie_producers mp ON p.id = mp.producers_id
+                    JOIN
+                movie m ON mp.movie_movie_id = m.movie_id
+            WHERE
+                m.winner = true
+        ),
+        differences AS (
+             SELECT
+                 producer_id,
+                 producer_name,
+                 movie_year,
+                 LAG(movie_year) OVER (PARTITION BY producer_id ORDER BY movie_year) AS previous_win
+             FROM
+                 victory_year
+        ),
+        intervals AS (
+            SELECT
+                 producer_id,
+                 producer_name,
+                 movie_year,
+                 previous_win,
+                 movie_year - previous_win AS inter
+            FROM
+                 differences
+            WHERE
+                 previous_win IS NOT NULL
+        )
+        SELECT
+            producer_name,
+            movie_year as following_win,
+            previous_win,
+            min(inter) as diff
+        FROM
+            intervals
+        group by
+            producer_id,
+            producer_name,
+            movie_year,
+            previous_win
+        having diff = select min(inter) from intervals""";
 
 
     private ApplicationConstants() {
